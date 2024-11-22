1c720428f6df297d41986decf1e5e2eba740dc0d
vishnu <vishnukp.me@gmail.com>
2024-11-23 04:00:57 +0530

    Added Signup form

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -5,10 +5,13 @@ import routes from "src/routes";
 
 import Dashboard from "components/Dashboard";
 
+import SignUp from "./components/Authentication/Signup";
+
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact component={Dashboard} path={routes.dashboard} />
+      <Route exact component={SignUp} path="/signup" />
     </Switch>
   </BrowserRouter>
 );
---
+++ app/javascript/src/components/Authentication/constants.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import * as yup from "yup";
+
+export const SIGNUP_FORM_INITIAL_VALUES = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  passwordConfirmation: "",
+};
+
+export const SIGNUP_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  email: yup
+    .string()
+    .email(i18n.t("messages.error.invalidEmail"))
+    .required("Required"),
+  password: yup.string().required("Required"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password")], i18n.t("messages.error.matchPassword"))
+    .required("Required"),
+});
