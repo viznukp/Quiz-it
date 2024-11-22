120d55ba9728fad1cdddec00c2659486820b4928
vishnu <vishnukp.me@gmail.com>
2024-11-22 20:22:22 +0530

    Added ability to expand and minimize sidebar

--- app/javascript/src/components/commons/Sidebar/SidebarNavItem.jsx
+++ app/javascript/src/components/commons/Sidebar/SidebarNavItem.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 
-import { Tooltip } from "neetoui";
+import classNames from "classnames";
+import { Tooltip, Typography } from "neetoui";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 
-import { isEmptyString } from "utils/stringUtils";
-
-const SidebarNavItem = ({ toolTipLabel = "", icon, onClickRoute = "" }) => (
-  <Tooltip content={toolTipLabel} disabled={isEmptyString(toolTipLabel)}>
-    <Link
-      className="flex h-8 w-8 items-center justify-center rounded-md transition-all ease-in hover:bg-gray-200"
-      to={onClickRoute}
+const SidebarNavItem = ({
+  label,
+  toolTipLabel = "",
+  toolTipEnabled = false,
+  icon,
+  onClickRoute = "",
+  onClickAction,
+  expanded = false,
+  active = false,
+  style = "link",
+}) => {
+  const renderContent = (
+    <div
+      className={classNames(
+        "flex h-10 items-center gap-3 rounded-md transition-all duration-500 ease-in-out hover:bg-gray-200",
+        { "justify-start border-b px-2": expanded },
+        { "w-10 justify-center": !expanded },
+        { "bg-blue-500": active }
+      )}
     >
       {icon}
-    </Link>
-  </Tooltip>
-);
+      {expanded && <Typography>{label}</Typography>}
+    </div>
+  );
+
+  return (
+    <Tooltip
+      content={toolTipLabel || label}
+      disabled={!toolTipEnabled || expanded}
+    >
+      {style === "link" ? (
+        <Link to={onClickRoute}>{renderContent}</Link>
+      ) : (
+        <button className="w-full" onClick={onClickAction}>
+          {renderContent}
+        </button>
+      )}
+    </Tooltip>
+  );
+};
 
 SidebarNavItem.propTypes = {
+  label: PropTypes.string.isRequired,
   toolTipLabel: PropTypes.string,
+  toolTipEnabled: PropTypes.bool,
   icon: PropTypes.node,
+  style: PropTypes.oneOf(["link", "button"]),
+  expanded: PropTypes.bool,
+  active: PropTypes.bool,
   onClickRoute: PropTypes.string,
+  onClickAction: PropTypes.func,
 };
 
 export default SidebarNavItem;
--- app/javascript/src/components/commons/Sidebar/index.jsx
+++ app/javascript/src/components/commons/Sidebar/index.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { List } from "neetoicons";
+import classNames from "classnames";
+import { List, RightArrow, LeftArrow } from "neetoicons";
 import { useTranslation } from "react-i18next";
 import routes from "src/routes";
 
-import { NeetoQuiz } from "neetoicons/app-icons";
-
+import QuizItLogo from "./QuizItLogo";
 import SidebarNavItem from "./SidebarNavItem";
 
 const Sidebar = () => {
   const { t } = useTranslation();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className="bordr-r flex w-16 flex-col items-center gap-3 border-r py-3">
-      <NeetoQuiz size={48} />
-      <div className="mt-4">
+    <div
+      className={classNames(
+        "flex w-16 flex-col items-center gap-3 border-r p-3 transition-all duration-500 ease-in-out",
+        { "w-64": isExpanded }
+      )}
+    >
+      <QuizItLogo isExpanded={isExpanded} />
+      <div className="mt-4 w-full">
         <SidebarNavItem
+          expanded={isExpanded}
+          style="button"
+          icon={
+            isExpanded ? (
+              <LeftArrow color="#ff0000" />
+            ) : (
+              <RightArrow color="#22C55E" />
+            )
+          }
+          onClickAction={() => setIsExpanded(!isExpanded)}
+        />
+        <SidebarNavItem
+          toolTipEnabled
+          expanded={isExpanded}
           icon={<List />}
-          toolTipLabel={t("labels.quizzes")}
+          label={t("labels.quizzes")}
           onClickRoute={routes.dashboard}
         />
       </div>
