710604ad81e785879a84103313c665397b6c0c07
vishnu <vishnukp.me@gmail.com>
2024-11-28 22:38:32 +0530

    Moved slug generation to service object

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -25,23 +25,8 @@ class Quiz < ApplicationRecord
   before_create :set_slug
 
   def set_slug
-    name_slug = name.parameterize
-    regex_pattern = "slug #{Constants::DB_REGEX_OPERATOR} ?"
-    latest_quiz_slug = Quiz.where(
-      regex_pattern,
-      "^#{name_slug}$|^#{name_slug}-[0-9]+$"
-    ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
-
-    slug_count = 0
-    if latest_quiz_slug.present?
-      slug_count = latest_quiz_slug.split("-").last.to_i
-      only_one_slug_exists = slug_count == 0
-      slug_count = 1 if only_one_slug_exists
-    end
-
-    slug_candidate = slug_count.positive? ? "#{name_slug}-#{slug_count + 1}" : name_slug
-
-    self.slug = slug_candidate
+    slug_service = SlugGeneratorService.new(self, :name, :slug)
+    self.slug = slug_service.generate_slug
   end
 
   private
