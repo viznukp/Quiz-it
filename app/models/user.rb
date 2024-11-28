46728d009921b948396068d14b3b3dd3c632c608
vishnu <vishnukp.me@gmail.com>
2024-11-28 20:54:01 +0530

    AAdded Organization creation while user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
     length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: VALID_EMAIL_REGEX }
   validates :user_type, presence: true, inclusion: { in: user_types.keys }
+  validates :organization_id, presence: true
 
   with_options if: :admin? do
     validates :password, presence: true, length: { minimum: MIN_PASSWORD_LENGTH }
