d0a74eab46096123a0518a1c051d58c5c8943b4d
vishnu <vishnukp.me@gmail.com>
2024-12-03 21:56:49 +0530

    Fixed unanswered count calculation

--- app/services/evaluation_service.rb
+++ app/services/evaluation_service.rb
@@ -4,33 +4,34 @@ class EvaluationService
   def evaluate_submission(submission)
     quiz = submission.quiz
     questions = quiz.questions
+    total_questions = questions.count
     correct_answers_count = 0
     wrong_answers_count = 0
-    unanswered_count = 0
 
     answer_key = {}
     questions.each do |question|
-      answer_key[question.id.to_s] = question.answer_index
+      answer_key[question.id] = {
+        answer_index: question.answer_index,
+        option_count: question.options.count
+      }
     end
 
     submission.answers.each do |answer|
       question_id = answer["question_id"]
       selected_choice = answer["selected_choice"]
-      correct_answer = answer_key[question_id]
+      correct_answer = answer_key[question_id][:answer_index]
 
-      if selected_choice == 0 || selected_choice.nil?
-        unanswered_count += 1
-      elsif selected_choice == correct_answer
+      if selected_choice == correct_answer
         correct_answers_count += 1
-      else
+      elsif (1...answer_key[question_id][:options_count]).include?(selected_choice)
         wrong_answers_count += 1
       end
     end
 
-    submission.total_questions = questions.count
+    submission.total_questions = total_questions
     submission.correct_answers_count = correct_answers_count
     submission.wrong_answers_count = wrong_answers_count
-    submission.unanswered_count = unanswered_count
+    submission.unanswered_count = total_questions - correct_answers_count + wrong_answers_count
     submission
   end
 end
