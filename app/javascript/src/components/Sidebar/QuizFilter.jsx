c56b5c77f2a7d89234c6bcd13debcae791931680
vishnu <vishnukp.me@gmail.com>
2024-12-05 03:25:26 +0530

    Added sidebar filter actions

--- app/javascript/src/components/Sidebar/QuizFilter.jsx
+++ app/javascript/src/components/Sidebar/QuizFilter.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 
+import { mergeLeft } from "ramda";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
 
 import { useFetchQuizStats } from "hooks/reactQuery/useQuizzesApi";
+import useQueryParams from "hooks/useQueryParams";
+import { buildUrl } from "utils/url";
 
 import SubNavItem from "./SubNavItem";
 
 const QuizFilter = ({ isVisible = false }) => {
   const { t } = useTranslation();
+  const queryParams = useQueryParams();
 
   const { data: { stats = {} } = {} } = useFetchQuizStats();
+
+  const history = useHistory();
+
+  const handleFilterSubmit = status =>
+    history.replace(buildUrl("", mergeLeft({ status }, queryParams)));
 
   return (
     isVisible && (
       <div className="flex flex-col gap-2 border-b py-2 pl-6">
-        <SubNavItem count={stats.totalQuizzes} label={t("labels.all")} />
+        <SubNavItem
+          count={stats.totalQuizzes}
+          label={t("labels.all")}
+          onClick={() => handleFilterSubmit("")}
+        />
         <SubNavItem
           count={stats.publishedQuizzes}
           label={t("labels.published")}
+          onClick={() => handleFilterSubmit("published")}
         />
-        <SubNavItem count={stats.draftQuizzes} label={t("labels.draft")} />
+        <SubNavItem
+          count={stats.draftQuizzes}
+          label={t("labels.draft")}
+          onClick={() => handleFilterSubmit("draft")}
+        />
       </div>
     )
   );
--- app/javascript/src/components/Sidebar/SubNavItem.jsx
+++ app/javascript/src/components/Sidebar/SubNavItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import { Button } from "neetoui";
 
-const SubNavItem = ({ label, count }) => (
+const SubNavItem = ({ label, count, onClick }) => (
   <div className="flex justify-between gap-2">
-    <Button label={label} style="text" />
+    <Button label={label} style="text" onClick={onClick} />
     <span className="rounded-lg bg-gray-100 p-2"> {count} </span>
   </div>
 );
