ecd9f8cc943d9bfeee510c100999951e9cbf0924
vishnu <vishnukp.me@gmail.com>
2024-12-06 04:53:53 +0530

    Modified searchbar to persist search term

--- app/javascript/src/components/commons/SearchBar.jsx
+++ app/javascript/src/components/commons/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Search as SearchIcon } from "neetoicons";
 import { Input } from "neetoui";
@@ -6,16 +6,21 @@ import PropTypes from "prop-types";
 
 import useFuncDebounce from "hooks/useFuncDebounce";
 
-const SearchBar = ({ placeholder = "", setSearchTerm }) => {
-  const [inputValue, setInputValue] = useState("");
-
+const SearchBar = ({ placeholder = "", searchTerm, setSearchTerm }) => {
   const debouncedSearch = useFuncDebounce(value => setSearchTerm(value));
+
+  useEffect(() => {
+    setInputValue(searchTerm || "");
+  }, [searchTerm]);
+
+  const [inputValue, setInputValue] = useState(searchTerm || "");
 
   return (
     <div>
       <Input
         placeholder={placeholder}
         prefix={<SearchIcon />}
+        type="search"
         value={inputValue}
         onChange={event => {
           const value = event.target.value;
@@ -29,6 +34,7 @@ const SearchBar = ({ placeholder = "", setSearchTerm }) => {
 
 SearchBar.propTypes = {
   placeholder: PropTypes.string,
+  searchTerm: PropTypes.string.isRequired,
   setSearchTerm: PropTypes.func.isRequired,
 };
 
