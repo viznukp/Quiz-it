2652d24ffa7da1857bb6d46a95a206f43503384b
vishnu <vishnukp.me@gmail.com>
2024-12-07 02:29:09 +0530

    Changed cutom dropdown in question display card to neetoui dropdown

--- app/javascript/src/components/QuestionBuilder/QuestionDisplayCard.jsx
+++ app/javascript/src/components/QuestionBuilder/QuestionDisplayCard.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { MenuHorizontal } from "neetoicons";
-import { Radio, Typography, Button } from "neetoui";
+import { Radio, Typography, Button, Dropdown } from "neetoui";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import routes from "src/routes";
@@ -18,7 +18,6 @@ const QuestionDisplayCard = ({
 }) => {
   const { t } = useTranslation();
   const history = useHistory();
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleQuestionDelete = async () => {
     try {
@@ -33,42 +32,35 @@ const QuestionDisplayCard = ({
     <div className="border p-4">
       <div className="relative flex justify-between">
         <Typography weight="semibold">{question}</Typography>
-        <Button
-          icon={MenuHorizontal}
-          style="text"
-          onClick={() => setIsOpen(!isOpen)}
-        />
-        {isOpen && (
-          <div className="absolute right-0 top-full z-10 mt-2 flex flex-col rounded-lg border bg-white p-3 shadow-lg">
-            <Button
-              label={t("labels.edit")}
-              style="text"
-              onClick={() =>
-                history.push(
-                  routes.quiz.question.edit
-                    .replace(":slug", slug)
-                    .replace(":id", id)
-                )
-              }
-            />
-            <Button
-              label={t("labels.clone")}
-              style="text"
-              onClick={() =>
-                history.push(
-                  routes.quiz.question.clone
-                    .replace(":slug", slug)
-                    .replace(":id", id)
-                )
-              }
-            />
-            <Button
-              label={t("labels.delete")}
-              style="danger-text"
-              onClick={handleQuestionDelete}
-            />
-          </div>
-        )}
+        <Dropdown buttonStyle="text" icon={MenuHorizontal}>
+          <Button
+            label={t("labels.edit")}
+            style="text"
+            onClick={() =>
+              history.push(
+                routes.quiz.question.edit
+                  .replace(":slug", slug)
+                  .replace(":id", id)
+              )
+            }
+          />
+          <Button
+            label={t("labels.clone")}
+            style="text"
+            onClick={() =>
+              history.push(
+                routes.quiz.question.clone
+                  .replace(":slug", slug)
+                  .replace(":id", id)
+              )
+            }
+          />
+          <Button
+            label={t("labels.delete")}
+            style="danger-text"
+            onClick={handleQuestionDelete}
+          />
+        </Dropdown>
       </div>
       <Radio stacked>
         {options?.map((option, index) => (
