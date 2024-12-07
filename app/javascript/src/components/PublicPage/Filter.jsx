9e175b127d7f426dbf2dd0503c01147cbbbc4e95
vishnu <vishnukp.me@gmail.com>
2024-12-07 17:30:06 +0530

    Improved Filter in public page

--- app/javascript/src/components/PublicPage/Filter.jsx
+++ app/javascript/src/components/PublicPage/Filter.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 import { Filter as FilterIcon } from "neetoicons";
-import { Typography, Button, Dropdown } from "neetoui";
+import { Typography, Dropdown } from "neetoui";
 import { mergeLeft } from "ramda";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
@@ -14,9 +14,7 @@ const Filter = () => {
   const { t } = useTranslation();
   const queryParams = useQueryParams();
   const history = useHistory();
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
   const filterRef = useRef(null);
-  const buttonRef = useRef(null);
 
   const { data: { categories = [] } = {} } = useFetchCategories();
 
@@ -24,44 +22,14 @@ const Filter = () => {
     history.replace(buildUrl("", mergeLeft({ category }, queryParams)));
   };
 
-  const handleClickOutside = event => {
-    if (
-      filterRef.current &&
-      !filterRef.current.contains(event.target) &&
-      event.target !== buttonRef.current
-    ) {
-      setIsFilterVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleFilterButtonClick = event => {
-    event.preventDefault();
-    setIsFilterVisible(prev => !prev);
-  };
-
   return (
-    <div className="relative">
-      <Button
-        icon={FilterIcon}
-        ref={buttonRef}
-        style="text"
-        tooltipProps={{ content: t("labels.filterByCategory") }}
-        onClick={handleFilterButtonClick}
-      />
-      {isFilterVisible && (
-        <div
-          className="absolute z-10 mt-2 flex w-48 flex-col gap-3 rounded-lg border bg-white px-4 py-3 shadow-xl"
-          ref={filterRef}
-        >
-          <Typography style="h4">{t("labels.category")}</Typography>
+    <Dropdown buttonStyle="text" icon={FilterIcon}>
+      <div
+        className="absolute z-10 mt-2 flex w-48 flex-col gap-3 rounded-lg border bg-white px-4 py-3 shadow-xl"
+        ref={filterRef}
+      >
+        <Typography style="h4">{t("labels.category")}</Typography>
+        <div onClick={event => event.stopPropagation()}>
           <Dropdown
             buttonStyle="secondary"
             className="border"
@@ -78,8 +46,8 @@ const Filter = () => {
             ))}
           </Dropdown>
         </div>
-      )}
-    </div>
+      </div>
+    </Dropdown>
   );
 };
 
