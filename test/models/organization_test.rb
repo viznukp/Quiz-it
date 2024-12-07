15eb8478d2bfe433feb55a628dea6bdb7323b31b
vishnu <vishnukp.me@gmail.com>
2024-12-08 04:01:55 +0530

    Added organization model tests

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -3,7 +3,27 @@
 require "test_helper"
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @organization = build(:organization)
+  end
+
+  def test_name_should_not_be_empty
+    @organization.name = ""
+    assert @organization.invalid?
+
+    assert_includes @organization.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_name_should_be_of_valid_length
+    @organization.name = "a" * (Organization::MAX_NAME_LENGTH + 1)
+    assert @organization.invalid?
+  end
+
+  def test_organization_should_not_be_saved_without_slug
+    @organization.save!
+
+    new_organization_with_existing_name = build(:organization)
+    new_organization_with_existing_name.name = @organization.name
+    assert new_organization_with_existing_name.save!, "Slug is already taken"
+  end
 end
