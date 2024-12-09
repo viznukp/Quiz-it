6423c08a3c517d91a46b7d0cb95d95af2d526b13
vishnu <vishnukp.me@gmail.com>
2024-12-09 19:54:06 +0530

    Added tooltips to icons

--- app/javascript/src/components/commons/ColumnFilter.jsx
+++ app/javascript/src/components/commons/ColumnFilter.jsx
@@ -3,8 +3,10 @@ import React, { useState } from "react";
 import { Column } from "neetoicons";
 import { Dropdown, Checkbox } from "neetoui";
 import { without } from "ramda";
+import { useTranslation } from "react-i18next";
 
 const ColumnFilter = ({ schema, setVisibleColumns }) => {
+  const { t } = useTranslation();
   const [columnsToHide, setColumnsToHide] = useState([]);
 
   const filterColumns = (schema, columnsToFilter) =>
@@ -20,7 +22,13 @@ const ColumnFilter = ({ schema, setVisibleColumns }) => {
   };
 
   return (
-    <Dropdown buttonStyle="text" icon={Column}>
+    <Dropdown
+      buttonStyle="text"
+      icon={Column}
+      buttonProps={{
+        tooltipProps: { content: t("labels.columnFilter"), position: "top" },
+      }}
+    >
       <div className="flex flex-col gap-3 p-4">
         {schema.map(
           ({ title, key, excludeFromColumnFilter, isDisabledInColumnFilter }) =>
