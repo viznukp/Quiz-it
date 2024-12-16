24ad3f9361a04be1bcc8cef70738807a5477158a
vishnu <vishnukp.me@gmail.com>
2024-12-16 21:33:21 +0530

    refactored create action of submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,19 +2,9 @@
 
 class SubmissionsController < ApplicationController
   skip_before_action :authenticate_user_using_x_auth_token, only: %i[create result ]
-  before_action :load_quiz, only: %i[create index result]
-  before_action :load_user, only: %i[create result]
 
   def create
-    existing_submission = Submission.find_by(user: @user, quiz: @quiz)
-    render_error(t("user_already_attempted_quiz"), :conflict) and return if existing_submission
-
-    submission = Submission.new(
-      user: @user, quiz: @quiz, status: submission_params[:status],
-      answers: submission_params[:answers])
-    submission = EvaluationService.new().evaluate_submission(submission)
-    submission.save!
-
+    EvaluationService.new(params).process!
     render_notice(t("submission_successfully_saved"))
   end
 
@@ -24,21 +14,9 @@ class SubmissionsController < ApplicationController
   end
 
   def result
-    result = ResultService.new.generate_result(Submission.find_by!(user: @user, quiz: @quiz))
+    quiz = Quiz.find_by!(slug: params[:slug])
+    user = User.find(params[:user_id])
+    result = ResultService.new.generate_result(Submission.find_by!(user:, quiz:))
     render_json(result)
   end
-
-  private
-
-    def submission_params
-      params.require(:submission).permit(:status, answers: [:question_id, :selected_choice])
-    end
-
-    def load_quiz
-      @quiz = Quiz.find_by!(slug: params[:slug])
-    end
-
-    def load_user
-      @user = User.find(params[:user_id])
-    end
 end
--- app/services/evaluation_service.rb
+++ app/services/evaluation_service.rb
@@ -1,37 +1,62 @@
 # frozen_string_literal: true
 
 class EvaluationService
-  def evaluate_submission(submission)
-    quiz = submission.quiz
-    questions = quiz.questions
-    total_questions = questions.count
-    correct_answers_count = 0
-    wrong_answers_count = 0
-
-    answer_key = {}
-    questions.each do |question|
-      answer_key[question.id] = {
-        answer_index: question.answer_index,
-        option_count: question.options.count
-      }
-    end
-
-    submission.answers.each do |answer|
-      question_id = answer["question_id"]
-      selected_choice = answer["selected_choice"]
-      correct_answer = answer_key[question_id][:answer_index]
-
-      if selected_choice == correct_answer
-        correct_answers_count += 1
-      elsif (1...answer_key[question_id][:options_count]).include?(selected_choice)
-        wrong_answers_count += 1
-      end
-    end
-
-    submission.total_questions = total_questions
-    submission.correct_answers_count = correct_answers_count
-    submission.wrong_answers_count = wrong_answers_count
-    submission.unanswered_count = total_questions - correct_answers_count - wrong_answers_count
-    submission
+  def initialize(params)
+    @params = params
   end
+
+  def process!
+    evaluate_submission
+  end
+
+  private
+
+    def evaluate_submission
+      quiz = Quiz.find_by!(slug: @params[:slug])
+      user = User.find(@params[:user_id])
+      @questions = quiz.questions
+      @submission = Submission.new(**submission_params, user:, quiz:)
+      calculate_answers
+      @submission.save!
+    end
+
+    def generate_answer_key
+      answer_key = {}
+      @questions.each do |question|
+        answer_key[question.id] = {
+          answer_index: question.answer_index,
+          option_count: question.options.count
+        }
+      end
+      answer_key
+    end
+
+    def calculate_answers
+      correct_answers_count = 0
+      wrong_answers_count = 0
+
+      answer_key = generate_answer_key
+
+      @submission.answers.each do |answer|
+        question_id = answer["question_id"]
+        selected_choice = answer["selected_choice"]
+        correct_answer = answer_key[question_id][:answer_index]
+
+        if selected_choice == correct_answer
+          correct_answers_count += 1
+        elsif (1...answer_key[question_id][:options_count]).include?(selected_choice)
+          wrong_answers_count += 1
+        end
+      end
+
+      total_questions = @questions.count
+      @submission.total_questions = total_questions
+      @submission.correct_answers_count = correct_answers_count
+      @submission.wrong_answers_count = wrong_answers_count
+      @submission.unanswered_count = total_questions - correct_answers_count - wrong_answers_count
+    end
+
+    def submission_params
+      @params.require(:submission).permit(:status, answers: [:question_id, :selected_choice])
+    end
 end
