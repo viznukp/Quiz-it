f8d995c0b208d9b9a3649d395940f0ec3fa9a1b2
vishnu <vishnukp.me@gmail.com>
2025-01-08 04:07:07 +0530

    Removed switch from QuizConfiguration component

--- app/javascript/src/components/QuizConfiguration/constants.jsx
+++ app/javascript/src/components/QuizConfiguration/constants.jsx
@@ -1,26 +1,39 @@
+import React from "react";
+
 import i18n from "i18next";
 import * as yup from "yup";
+
+import Dashboard from "./Dashboard";
+import EmailNotification from "./EmailNotification";
+import QuizRandomizer from "./QuizRandomizer";
+import QuizTime from "./QuizTime";
+import Visibility from "./Visibility";
 
 export const CONFIGURATION_PANELS = {
   dashboard: {
     id: "dashboard",
     title: i18n.t("labels.quizSettings"),
+    panel: props => <Dashboard {...props} />,
   },
   visibility: {
     id: "visibility",
     title: i18n.t("configurationCards.visibility.title"),
+    panel: props => <Visibility {...props} />,
   },
   quizTiming: {
     id: "quizTiming",
     title: i18n.t("configurationCards.timing.title"),
+    panel: props => <QuizTime {...props} />,
   },
   emailNotification: {
     id: "emailNotification",
     title: i18n.t("configurationCards.emailNotifications.title"),
+    panel: props => <EmailNotification {...props} />,
   },
   questionsAndOptions: {
     id: "questionsAndOptions",
     title: i18n.t("configurationCards.questionsAndOptions.title"),
+    panel: props => <QuizRandomizer {...props} />,
   },
 };
 
--- app/javascript/src/components/QuizConfiguration/index.jsx
+++ app/javascript/src/components/QuizConfiguration/index.jsx
@@ -7,11 +7,6 @@ import { Container, NavBar, ContentWrapper } from "components/commons";
 import { QUIZ_TABS, QUIZ_TAB_IDS } from "components/constants";
 
 import { CONFIGURATION_PANELS } from "./constants";
-import Dashboard from "./Dashboard";
-import EmailNotification from "./EmailNotification";
-import QuizRandomizer from "./QuizRandomizer";
-import QuizTime from "./QuizTime";
-import Visibility from "./Visibility";
 
 const QuizConfiguration = () => {
   const { slug } = useParams();
@@ -32,26 +27,7 @@ const QuizConfiguration = () => {
         title={quiz?.name}
       />
       <ContentWrapper>
-        {(() => {
-          switch (activePanel.id) {
-            case CONFIGURATION_PANELS.dashboard.id:
-              return <Dashboard setActivePanel={setActivePanel} />;
-            case CONFIGURATION_PANELS.visibility.id:
-              return <Visibility setActivePanel={setActivePanel} {...quiz} />;
-            case CONFIGURATION_PANELS.quizTiming.id:
-              return <QuizTime setActivePanel={setActivePanel} {...quiz} />;
-            case CONFIGURATION_PANELS.emailNotification.id:
-              return (
-                <EmailNotification setActivePanel={setActivePanel} {...quiz} />
-              );
-            case CONFIGURATION_PANELS.questionsAndOptions.id:
-              return (
-                <QuizRandomizer setActivePanel={setActivePanel} {...quiz} />
-              );
-            default:
-              return null;
-          }
-        })()}
+        {activePanel.panel({ ...quiz, setActivePanel })}
       </ContentWrapper>
     </Container>
   );
